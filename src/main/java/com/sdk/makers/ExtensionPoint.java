5c7a6429943b7fff22673f52be90d801b84f6f49
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2019-01-16 08:43:00 +0100

    Updating config adding of ExtensionPoint

--- src/main/java/com/sdk/makers/ExtensionPoint.java
+++ src/main/java/com/sdk/makers/ExtensionPoint.java
@@ -2,10 +2,19 @@ package com.sdk.makers;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class ExtensionPoint {
 
+    private Map<ConfigType, JSONObject> configFiles = new HashMap<>();
+
+    public void addConfig(ConfigType configType, JSONObject jsonObject) {
+        configFiles.put(configType, jsonObject);
+    }
+
     public JSONObject getConfigFile(ConfigType configType) {
-        return null;
+        return configFiles.get(configType);
     }
 
     public enum ConfigType {
